284fe4c396ed04957599c287e63701fbbb331951
a20sarle <a20sarle.student@his.se>
2021-04-01 10:24:32 +0200

    snackbar msg changed

--- app/src/main/java/com/example/firstandroidapplication/MainActivity.java
+++ app/src/main/java/com/example/firstandroidapplication/MainActivity.java
@@ -26,7 +26,7 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "Nu är texten utbytt", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
